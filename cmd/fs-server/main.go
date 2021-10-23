24b2c67d4244f8c9efa69ea284635b549559567d
Peyman Mortazavi <peyman.mortazavi@mariadb.com>
2021-10-23 11:59:16 -0400

    add server

--- cmd/fs-server/main.go
+++ cmd/fs-server/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/peymanmortazavi/fs-server/pkg/filesystem"
+	"github.com/peymanmortazavi/fs-server/pkg/fshttp"
+)
+
 func main() {
-	manager := filesystem.DirManager{""}
+	rootDir := flag.String("root", "", "the root of the local path to serve.")
+	addr := flag.String("addr", "0.0.0.0:6000", "the address to listen to (default: 0.0.0.0:6000)")
+
+	flag.Parse()
+
+	manager := filesystem.DirManager{Root: *rootDir}
+	handler := &fshttp.Handler{Viewer: manager}
+	http.Handle("/", handler)
+
+	log.Fatalln(http.ListenAndServe(*addr, nil))
 }
